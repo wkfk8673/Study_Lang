cfb6ec04f9e77433b2217648fca163edf437e26b
Kang Minyoung <kangttam@gmail.com>
2025-05-28 18:09:38 +0900

    [ADD] Ch2 and Ch3
    -- PickRandomCard
    -- ExperimentWithControls

---
+++ C_Sharp/Head_First_C#/ChapterThree/Chapter3/Chapter3/CardPicker.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Markup;
+
+namespace PickRandomCards
+{
+    private class PickSomeCards
+    {
+        private int RandomValue()
+        {
+            if ()
+            {
+                return 
+            }
+        }
+        private void RandomSuit(int value) 
+        { 
+        
+        }
+    }
+}
--- C_Sharp/Head_First_C#/ChapterTwo/ExperimentWithControls/ExperimentWithControls/MainWindow.xaml.cs
+++ C_Sharp/Head_First_C#/ChapterTwo/ExperimentWithControls/ExperimentWithControls/MainWindow.xaml.cs
@@ -30,5 +30,47 @@ namespace ExperimentWithControls
         {
             e.Handled = !int.TryParse(e.Text, out int result); // 숫자가 아닌 모든 텍스트를 무시
         }
+
+        private void Slider_ValueChanged(object sender, RoutedPropertyChangedEventArgs<double> e)
+        {
+            number.Text = smallSlider.Value.ToString("0");
+        }
+
+        private void bigSlider_ValueChanged(object sender, RoutedPropertyChangedEventArgs<double> e)
+        {
+            number.Text = bigSlider.Value.ToString("000-000-0000"); //전화번호 형태로 형식화
+        }
+
+        private void RadioButton_Checked(object sender, RoutedEventArgs e)
+        {
+            if (sender is RadioButton radioButton)
+            {
+                number.Text = radioButton.Content.ToString();
+            }
+        }
+
+        private void myListBox_SelectionChanged(object sender, SelectionChangedEventArgs e)
+        {
+            if(myListBox.SelectedItem is ListBoxItem listBoxItem)
+            {
+                number.Text = listBoxItem.Content.ToString();
+            }
+        }
+
+        private void readOnlyComboBox_SelectionChanged(object sender, SelectionChangedEventArgs e)
+        {
+            if(readOnlyComboBox.SelectedItem is ListBoxItem listBoxItem)
+            {
+                number.Text = listBoxItem.Content.ToString();
+            }
+        }
+
+        private void editableComboBox_TextChanged(object sender, TextChangedEventArgs e)
+        {
+            if(sender is ComboBox comboBox)
+            {
+                number.Text = comboBox.Text;
+            }
+        }
     }
 }
