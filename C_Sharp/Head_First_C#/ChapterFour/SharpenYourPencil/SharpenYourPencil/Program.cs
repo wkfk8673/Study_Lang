7761addc3c6c26df997a0b9ef016692dfd5bae1b
Kang Minyoung <kangttam@gmail.com>
2025-06-14 02:34:31 +0900

    [ADD] CH4
    -- example

---
+++ C_Sharp/Head_First_C#/ChapterFour/SharpenYourPencil/SharpenYourPencil/Program.cs
@@ -0,0 +1,65 @@
+﻿namespace SharpenYourPencil
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Elephant lucinda = new Elephant() { Name = "Lucinda", EarSize = 33};
+            Elephant lloyd = new Elephant() { Name = "Lloyd", EarSize = 40};
+
+            Console.WriteLine("Press 1 for Lloyd, 2 for Lucinda, 3 to swap");
+            while (true)
+            {
+                char input = Console.ReadKey(true).KeyChar;
+                Console.WriteLine($"You Pressed {input}");
+                if (input == '1')
+                {
+                    lloyd.whoAmI();
+                }
+                else if (input == '2')
+                {
+                    lucinda.whoAmI();
+                }
+                else if (input == '3') // swap
+                {
+                    Elephant holder;
+                    holder = lloyd;
+                    lloyd = lucinda;
+                    lucinda = holder;
+
+                    Console.WriteLine("References have been swapped");
+                }
+                else if (input == '5')
+                {
+                    lucinda.SpeakTo(lloyd, "Hi, Lloyd!");
+                }
+                else return;
+                Console.WriteLine();
+            }
+        }
+    }
+
+    class Elephant
+    {
+        public int EarSize;
+        public string Name;
+
+        public void whoAmI()
+        {
+            Console.WriteLine($"Calling {Name}.WhoAmI()");
+            Console.WriteLine($"My name is {Name}.");
+            Console.WriteLine($"My ears are {EarSize} inches tall.");
+        }
+
+        public void HearMessage(string message, Elephant whoSaidIt)
+        {
+            Console.WriteLine($"{Name} heard a message");
+            Console.WriteLine($"{whoSaidIt.Name} said this : {message}");
+        }
+
+        public void SpeakTo(Elephant whoToTalkTo, string message)
+        {
+            whoToTalkTo.HearMessage(message, this);
+        }
+    }
+}
--- C_Sharp/Head_First_C#/ChapterFour/SharpenYourPencil/SharpenYourPencil/obj/Debug/net8.0/SharpenYourPencil.AssemblyInfo.cs
+++ C_Sharp/Head_First_C#/ChapterFour/SharpenYourPencil/SharpenYourPencil/obj/Debug/net8.0/SharpenYourPencil.AssemblyInfo.cs
@@ -11,12 +11,12 @@
 using System;
 using System.Reflection;
 
-[assembly: System.Reflection.AssemblyCompanyAttribute("AbilityScoreCalculator")]
+[assembly: System.Reflection.AssemblyCompanyAttribute("SharpenYourPencil")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
 [assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+fe0da192a14e22b6fa906f7b31f84485d51601f5")]
-[assembly: System.Reflection.AssemblyProductAttribute("AbilityScoreCalculator")]
-[assembly: System.Reflection.AssemblyTitleAttribute("AbilityScoreCalculator")]
+[assembly: System.Reflection.AssemblyProductAttribute("SharpenYourPencil")]
+[assembly: System.Reflection.AssemblyTitleAttribute("SharpenYourPencil")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
 
 // MSBuild WriteCodeFragment 클래스에서 생성되었습니다.
